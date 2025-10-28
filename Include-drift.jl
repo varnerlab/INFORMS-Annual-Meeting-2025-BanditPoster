c1143edf1caa9b6689cdf96a8e31774bf2cb7447
Jeffrey Varner <jdv27@cornell.edu>
2025-10-28 09:06:16 -0400

    Add SIM min variance and portfolio drift examples
    Added CHEME-5660-L8b-Example-SIM-MinVar-RRFA-Fall-2025.ipynb and CHEME-5660-L9a-Example-Portfolio-Drift-Fall-2025.ipynb for portfolio optimization using the single index model and portfolio drift analysis. Updated CHEME-5660-L9a-Example-INFORMS-Fall-2025.ipynb. Added Include-drift.jl for environment setup. Added Poster-Calculations-INFORMS-Fall-2025.ipynb. Moved and updated poster and latex assets to poster/latex/. Minor updates to poster/Poster.key.

---
+++ Include-drift.jl
@@ -0,0 +1,35 @@
+# setup paths -
+const _ROOT = pwd();
+const _PATH_TO_SRC = joinpath(_ROOT, "src");
+const _PATH_TO_DATA = joinpath(_ROOT, "data");
+
+# make sure all is up to date -
+using Pkg
+if (isfile(joinpath(_ROOT, "Manifest.toml")) == false) # have manifest file, we are good. Otherwise, we need to instantiate the environment
+    Pkg.add(path="https://github.com/varnerlab/VLQuantitativeFinancePackage.jl.git")
+    Pkg.activate("."); Pkg.resolve(); Pkg.instantiate(); Pkg.update();
+end
+
+# load external packages -
+using VLQuantitativeFinancePackage
+using DataFrames
+using CSV
+using Dates
+using LinearAlgebra
+using Statistics
+using Plots
+using Colors
+using StatsPlots
+using JLD2
+using FileIO
+using Distributions
+using Distances
+using MathOptInterface
+using DataStructures
+using PrettyTables
+using StatsBase
+using KernelFunctions
+using HypothesisTests
+using JuMP
+using MadNLP
+using NNlib
